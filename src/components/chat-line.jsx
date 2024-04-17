6201cb40185c25c964cd31d48a0e1047d6f69940
alefaso-lucky <150935261+alefaso-lucky@users.noreply.github.com>
2024-04-17 12:48:32 +0200

    icona avatar AI

--- src/components/chat-line.jsx
+++ src/components/chat-line.jsx
@@ -9,13 +9,13 @@ import styles from './chat-line.module.css';
 import Avatar from '@mui/material/Avatar';
 import MedicalInformationTwoToneIcon from '@mui/icons-material/MedicalInformationTwoTone';
 import { cyan } from '@mui/material/colors';
-import SettingsSuggestTwoToneIcon from '@mui/icons-material/SettingsSuggestTwoTone';
+import TipsAndUpdatesTwoToneIcon from '@mui/icons-material/TipsAndUpdatesTwoTone';
 
 export default function ChatLine({message}) {
   return (
     <Card className={styles.total}>
       <CardActionArea className={message instanceof HumanMessage ? styles.contDoctor : styles.contAi}>
-      {message instanceof HumanMessage ? "" : <Avatar className={styles.AvatarAI} sx={{ bgcolor: cyan.A700 }}><SettingsSuggestTwoToneIcon/></Avatar>}
+      {message instanceof HumanMessage ? "" : <Avatar className={styles.AvatarAI} sx={{ bgcolor: cyan.A700 }}><TipsAndUpdatesTwoToneIcon/></Avatar>}
       
         <CardContent className={message instanceof HumanMessage ? styles.doctor : styles.ai}>
           <h3>{message instanceof HumanMessage ? ("Doctor") : ("AI")}</h3>
