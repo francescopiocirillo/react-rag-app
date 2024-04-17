6d499abdc70adc6f28adff0f04cf6c5e5c1526a8
alefaso-lucky <150935261+alefaso-lucky@users.noreply.github.com>
2024-04-17 12:46:31 +0200

    avatar e altri dettagli per i messaggi singoli

--- src/components/chat-line.jsx
+++ src/components/chat-line.jsx
@@ -5,19 +5,26 @@ import CardContent from '@mui/material/CardContent';
 import { CardActionArea } from '@mui/material';
 import { HumanMessage, AIMessage } from "@langchain/core/messages";
 import { blue } from '@mui/material/colors';
-import styles from './chat-line.module.css'
-
+import styles from './chat-line.module.css';
+import Avatar from '@mui/material/Avatar';
+import MedicalInformationTwoToneIcon from '@mui/icons-material/MedicalInformationTwoTone';
+import { cyan } from '@mui/material/colors';
+import SettingsSuggestTwoToneIcon from '@mui/icons-material/SettingsSuggestTwoTone';
 
 export default function ChatLine({message}) {
   return (
-    <Card>
-      <CardActionArea>
+    <Card className={styles.total}>
+      <CardActionArea className={message instanceof HumanMessage ? styles.contDoctor : styles.contAi}>
+      {message instanceof HumanMessage ? "" : <Avatar className={styles.AvatarAI} sx={{ bgcolor: cyan.A700 }}><SettingsSuggestTwoToneIcon/></Avatar>}
+      
         <CardContent className={message instanceof HumanMessage ? styles.doctor : styles.ai}>
           <h3>{message instanceof HumanMessage ? ("Doctor") : ("AI")}</h3>
           <p>
             {message.content}
           </p>
         </CardContent>
+        {message instanceof HumanMessage ? <Avatar className={styles.AvatarDoc} sx={{ bgcolor: cyan[100] }} ><MedicalInformationTwoToneIcon/></Avatar> : "" }
+      
       </CardActionArea>
     </Card>
   );
